4ac236f90aa8b2fc22ac02642754c1f7fd2243e6
elios-cama <elios.cama@gmail.com>
2021-01-21 17:00:03 +0100

    bottom and appbar

---
+++ lib/HomePage.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage({Key key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        image : DecorationImage(
+          image: AssetImage("lib/assets/gradient.png"),
+          fit: BoxFit.cover
+          )
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          actions: [
+            Padding(
+              padding: const EdgeInsets.only(top: 30, right: 15),
+              child: ClipRRect(
+                
+                borderRadius: BorderRadius.circular(15.0),
+                child: Image.asset(
+                  'lib/assets/photomdb.jpg', 
+                  height: 50,
+                  width: 50,
+                  fit: BoxFit.cover,
+                  ),
+                
+              ),
+            )
+          ],
+          elevation: 0,
+          title: Padding(
+            padding: const EdgeInsets.only(top: 30),
+            child: Text("Today's schedule", style: TextStyle(fontSize: 37, fontWeight : FontWeight.bold, color : Colors.white)),
+          ),
+          backgroundColor: Colors.transparent,
+          toolbarHeight: 80,
+        ),
+        bottomNavigationBar: BottomAppBar(
+          elevation: 0,
+          color: Colors.transparent,
+          child: Padding(
+            padding: const EdgeInsets.all(30.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children : <Widget>[
+                Icon(Icons.house, color: Colors.white, size: 45,),
+                CircleAvatar(
+                  radius: 30,
+                  child: Icon(Icons.add, color: Colors.white,size: 35),
+                  backgroundColor: Colors.purple.shade400,
+                  ),
+                Icon(Icons.settings, color: Colors.white,size: 45)
+              ]
+            ),
+          )
+  ),
+      ),
+    );
+  }
+}
